8a0fa0f7c968d58707d71eae994794efac2db111
HawkLindner <hm29lind@siena.edu>
2024-04-11 11:41:24 -0400

    worked on

--- Cube/main.js
+++ Cube/main.js
@@ -13,15 +13,23 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+const sphere = new THREE.SphereGeometry(10,64,32);
+const sphereMat = new THREE.MeshBasicMaterial({color:"red"});
+const sphereObj = new THREE.Mesh(sphere,sphereMat);
+scene.add(sphereObj);
+
+
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	cube.rotation.x += 0.05;
+	cube.rotation.y += 0.05;
 
 	renderer.render( scene, camera );
 }
 
 animate();
+
+THREE.AnimationMixer(scene,cube)
