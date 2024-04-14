2abc6833a6697975b0e55b1d8460dc47cf148dae
hawk <hm29lind@siena.edu>
2024-04-14 15:05:21 -0400

    fixed clear, and shuffle

--- cassino games/blackjack.js
+++ cassino games/blackjack.js
@@ -1,5 +1,21 @@
 
 let deck;
+let  userSum = 0;
+let dealerSum= 0;
+let checkAce = 0;
+let userAce = false;
+let dealerAce = false;
+let userWin = false;
+let dealerIn = false;
+let userBust = false;
+let dealerBust = false;
+let canHit = true;
+let onTable = [];
+let cardCount = 1;
+let delt;
+let playerHand = document.getElementById("pCards");
+let dealerHand = document.getElementById("dCards");
+let winText = document.getElementById("winnerText");
 function buildDeck(){
     let value = ["A", "2", "3", "4", "5", "6", "7", "8","9","10","J","Q","K"];
     let suit = ["C","S","H","D"];
@@ -19,48 +35,32 @@ function shuffleDeck(){
         let temp = deck[i];
         deck[i] = deck[j];
         deck[j] = temp;
+        console.log("shuffle");
     }
     //(deck);
 }
-let onTable = [];
-let cardCount = 1;
-let delt;
-
 function clearTable(){
     let dealerCards = document.getElementById("dCards");
     let userCards = document.getElementById("pCards")
-
-    for(i = 0 ; i < cardCount;i++){
-        dealerCards.removeChild(test.firstChild);
+    while(dealerCards.firstChild){
+        dealerCards.removeChild(dealerCards.firstChild);
     }
-    cardCount = 0;
+    while(userCards.firstChild){
+        userCards.removeChild(userCards.firstChild);
+    }
+    
+    cardCount = 1;
     //console.log("in");
     delt = document.createElement("img");
+    winText.innerText = "";
+    userSum = 0;
+    dealerSum = 0;
     buildDeck();
     shuffleDeck();
+    console.log("Clear");
 }
-
-window.onload = function(){
-    buildDeck();
-    shuffleDeck();
-    startGame();
-}
-
-
-
-
-let shuffleBtn = document.getElementById("shuffle");
-shuffleBtn.addEventListener("click",shuffleDeck);
-
-
-let clear = document.getElementById("clear");
-clear.addEventListener("click",clearTable);
-
-let hitBtn = document.getElementById("hit");
-hitBtn.addEventListener("Click",hit);
-
-
 function hit(){
+    onTable[cardCount] = document.createElement("img");
     //to hit we want to add cards to user hand
     onTable[cardCount].id = deck.pop();
     onTable[cardCount].src = "/cassino games/cards/"+onTable[cardCount].id+".png";
@@ -69,10 +69,24 @@ function hit(){
         userAce = true;
     }
     onTable[cardCount].value = checkAce;
-    userSum = userSum + onTable[cardCount].value; 
+    userSum = parseInt(userSum) + parseInt(onTable[cardCount].value); 
     playerHand.append(onTable[cardCount]);
+
+    if(checkWin() == false){
+        hitBtn.disabled = true;
+    }
+    console.log(userSum);
     cardCount++;
+    console.log("Hit");
     
+}
+function checkWin(){
+    if(userSum == 21){
+        //user Wins
+    }
+    else if(dealerSum > userSum && dealerSum <22){
+        //dealerwins
+    }
 }
 //creating the game
 
@@ -80,20 +94,6 @@ function hit(){
 //then the player gets one card that is showing
 //then the dealer gets one card showing
 //then the player gets one card showing
-let userSum = 0;
-let dealerSum= 0;
-let checkAce = 0;
-let userAce = false;
-let dealerAce = false;
-let userWin = false;
-let dealerIn = false;
-let userBust = false;
-let dealerBust = false;
-let canHit = true;
-let playerHand = document.getElementById("pCards");
-let dealerHand = document.getElementById("dCards");
-let winText = document.getElementById("winnerText");
-
 function startGame(){
     onTable[cardCount] = document.createElement("img");
     onTable[cardCount].src = "BackOfCard.jpg";
@@ -145,6 +145,7 @@ function startGame(){
     dealerSum = 1*onTable[1].value + 1*onTable[3].value;
     userSum = 1*onTable[2].value + 1*onTable[4].value;
     
+    console.log(userSum);
     if(dealerSum == 21){
         onTable[1].src = "/cassino games/cards/"+onTable[1].id+".png";
         if(userSum == 21){
@@ -158,12 +159,14 @@ function startGame(){
         onTable[1].src = "/cassino games/cards/"+onTable[1].id+".png";
         winText.append("You win!");
     }
-
+    //console.log(cardCount);
 
 }
 //For next log on we need to finish up with setting up clear, hit and the score tracking
 function checkValue(card){
+    console.log(card);
     let value = card.id.split("-");
+    console.log(value);
     if(isNaN(value[0])){
         if(value[0] === 'A'){
             return card.value = 11;
@@ -186,6 +189,35 @@ function checkBust(value){
         return true;
     }
 }
+
+
+window.onload = function(){
+    shuffleDeck();
+}
+
+let shuffleBtn = document.getElementById("shuffle");
+shuffleBtn.addEventListener("click",shuffleDeck);
+
+let clear = document.getElementById("clear");
+clear.addEventListener("click",clearTable);
+
+let hitBtn = document.getElementById("hit");
+hitBtn.addEventListener("click",hit);
+
+let start = document.getElementById("start");
+start.addEventListener("click",startGame);
+
+
+
+
+
+
+
+
+
+
+
+
 
 //after this we add the sum for the player and ask to hit or stand. If number is greater than 21, they lose
 //if the user stays we needs to reveal dealer card and add values
