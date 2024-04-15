a82c990e0f6bc7629b4d1ee44d32b086f28a21b5
HawkLindner <hm29lind@siena.edu>
2024-04-15 00:27:19 -0400

    worked more on my blackjack

--- cassino games/blackjack.js
+++ cassino games/blackjack.js
@@ -13,17 +13,19 @@ let canHit = true;
 let onTable = [];
 let cardCount = 1;
 let delt;
-let stay = false;
+let stay = false;   //stay starts at false
 let win = false; //false if house wins
+let dealerCheck = true;
 let playerHand = document.getElementById("pCards");
 let dealerHand = document.getElementById("dCards");
 let winText = document.getElementById("winnerText");
-function buildDeck(){
-    let value = ["A", "2", "3", "4", "5", "6", "7", "8","9","10","J","Q","K"];
-    let suit = ["C","S","H","D"];
-    deck = [];
 
-    for( i = 0; i < suit.length; i++){
+
+function buildDeck(){
+    let value = ["A", "2", "3", "4", "5", "6", "7", "8","9","10","J","Q","K"];     //builds the number cards, Ace, 1-10, Jack, Queen, King
+    let suit = ["C","S","H","D"];                                                  //suits of cards in the deck. Each card gets 4
+    deck = [];                                                                     //creates an empty deck array
+    for( i = 0; i < suit.length; i++){                                             //sets the array with the value, a dash and a suit
         for(j = 0; j < value.length; j++){
             deck.push(value[j] +"-" +suit[i]);
         }
@@ -34,37 +36,37 @@ function shuffleDeck(){
     for(i = 0 ; i < deck.length;i++){
         //need to store the current card and get a random card
         let j = Math.floor(Math.random()*deck.length);//0-1 * deck length
+        //this switches 2 random cards in the deck
         let temp = deck[i];
         deck[i] = deck[j];
         deck[j] = temp;
     }
-    //(deck);
 }
 function clearTable(){
-    let dealerCards = document.getElementById("dCards");
-    let userCards = document.getElementById("pCards");
-    stay = false;
-    while(dealerCards.firstChild){
-        dealerCards.removeChild(dealerCards.firstChild);
-    }
-    while(userCards.firstChild){
-        userCards.removeChild(userCards.firstChild);
-    }
-    
+    //reset buttons
+    startBtn.disabled = false;
+    shuffleBtn.disabled = false;
+    hitBtn.disabled = true;
+    clearBtn.disabled = true;
+    stayBtn.disabled = true;
     cardCount = 1;
     delt = document.createElement("img");
     winText.innerText = "";
     userSum = 0;
     dealerSum = 0;
-    startBtn.disabled = false;
-    shuffleBtn.disabled = false;
-    hitBtn.disabled = true;
-    clearBtn.disabled = true;
-    stayBtn.disabled = false;
-    buildDeck();
+
+    //remove the children of the LI
+    while(dealerHand.firstChild){
+        dealerHand.removeChild(dealerHand.firstChild);
+    }
+    while(playerHand.firstChild){
+        playerHand.removeChild(playerHand.firstChild);
+    }
+    
+    //call a shuffle deck
     shuffleDeck();
 }
-userCheck = true;
+
 function hit(){
     
     onTable[cardCount] = document.createElement("img");
@@ -77,11 +79,11 @@ function hit(){
     onTable[cardCount].value = checkAce;
     userSum = parseInt(userSum) + parseInt(onTable[cardCount].value); 
     playerHand.append(onTable[cardCount]);
-
+    //call a check score function
+    check();
     cardCount++;
-    userCheck = check();
 }
-    
+
 
 //creating the game
 
@@ -90,55 +92,66 @@ function hit(){
 //then the dealer gets one card showing
 //then the player gets one card showing
 function startGame(){
-    startBtn.disabled = true;
-    shuffleBtn.disabled = true;
-    clearBtn.disabled = false;
-    hitBtn.disabled = false;
-    stayBtn.disabled = false;
-    stay = false;
-    onTable[cardCount] = document.createElement("img");
-    onTable[cardCount].src = "BackOfCard.jpg";
-    onTable[cardCount].id = deck.pop();
-    checkAce = checkValue(onTable[cardCount]);
+    //on start of the game 
+    startBtn.disabled = true;   //disable start btn
+    shuffleBtn.disabled = true; //disable shuffle btn
+    clearBtn.disabled = false;  //enable clear btn
+    hitBtn.disabled = false;    //enable hit btn   
+    stayBtn.disabled = false;   //disable stay btn
+    stay = false;               //set stay to true
+
+
+    //creating the card that is flipped over in the dealers hand onTable[1]
+    onTable[cardCount] = document.createElement("img"); //creates the img tag in html
+    onTable[cardCount].src = "BackOfCard.jpg";          //sets the img of the card to the back of a card
+    onTable[cardCount].id = deck.pop();                 //stores first element in deck to onTable[1];
+    checkAce = checkValue(onTable[cardCount]);          //checks the method check value to see if it is an ace. if so then dealer ace == true, else value is stored
     if(checkAce === 11){
         dealerAce = true;
     }
-    dealerSum = onTable[cardCount].value;
-    dealerHand.append(onTable[cardCount]);
-    cardCount++;
+    onTable[cardCount].value = checkAce;                //set value for onTable[1] to whatever check ace returned
+    dealerSum = parseInt(onTable[cardCount].value);               //set the first part of dealer sum to what onTable[1] value is
+    dealerHand.append(onTable[cardCount]);              //append the card to the LI element
+    cardCount++;                                        //incroment through card count for next card
+    
+    //card one for user hand, onTable[2]
+    onTable[cardCount] = document.createElement("img"); //creates a img element for the card to the user
+    onTable[cardCount].id = deck.pop();                 //pops the top card off decks and stores it as the id for onTable[2].id
+    onTable[cardCount].src = "/cassino games/cards/"+onTable[cardCount].id+".png";  //stores the src for the img as the card from the folder
+    checkAce = checkValue(onTable[cardCount]);          //check ace function as used previously
+    if(checkAce === 11){
+        userAce = true;
+    }
+    onTable[cardCount].value = checkAce;                //set valuie for onTable[2] to check ace value
+    userSum = parseInt(onTable[cardCount].value);       //set the user sum to the value
+    playerHand.append(onTable[cardCount]);              //append this to playerhand as an child element
+    cardCount++;                                        //increase card count to 3
     
 
-    onTable[cardCount] = document.createElement("img");
-    onTable[cardCount].id = deck.pop();
-    onTable[cardCount].src = "/cassino games/cards/"+onTable[cardCount].id+".png";
-    checkAce = checkValue(onTable[cardCount]);
-    if(checkAce === 11){
-        userAce = true;
-    }
-    onTable[cardCount].value = checkAce;
-    playerHand.append(onTable[cardCount]);
-    cardCount++;
-
-    onTable[cardCount] = document.createElement("img");
-    onTable[cardCount].id = deck.pop();
-    onTable[cardCount].src = "/cassino games/cards/"+onTable[cardCount].id+".png";
-    checkAce = checkValue(onTable[cardCount]);
+    //dealer card 2, onTable[3]
+    onTable[cardCount] = document.createElement("img"); //creates img tag
+    onTable[cardCount].id = deck.pop();                 //pops off the top card and stores
+    onTable[cardCount].src = "/cassino games/cards/"+onTable[cardCount].id+".png";  //sets img png for screen
+    checkAce = checkValue(onTable[cardCount]);          //check ace function as used previously
     if(checkAce === 11){
         dealerAce = true;
-    }
-    onTable[cardCount].value = checkAce;
-    dealerSum = dealerSum + onTable[cardCount].value;
-    dealerHand.append(onTable[cardCount]);
-    cardCount++;
+    }       
+    onTable[cardCount].value = checkAce;                //set the value of onTable[cardCound] to checkAce
+    dealerSum = parseInt(dealerSum) + parseInt(onTable[cardCount].value);   //add to dealerSum
+    dealerHand.append(onTable[cardCount]);              //append the card to the LI html element
+    cardCount++;                                        //incroment
+    
 
-    onTable[cardCount] = document.createElement("img");
-    onTable[cardCount].id = deck.pop();
-    onTable[cardCount].src = "/cassino games/cards/"+onTable[cardCount].id+".png";
-    checkAce = checkValue(onTable[cardCount]);
+    //creates card 3, on table[4]
+    onTable[cardCount] = document.createElement("img");     //create an img tag
+    onTable[cardCount].id = deck.pop();                     //store the id of CardCount[4] as
+    onTable[cardCount].src = "/cassino games/cards/"+onTable[cardCount].id+".png";  //set the src of the img on onTable[4]
+    checkAce = checkValue(onTable[cardCount]);              //check ace as earlier
     if(checkAce === 11){
         userAce = true;
     }
-    onTable[cardCount].value = checkAce;
+    onTable[cardCount].value = checkAce;                    //sets the value of onTable[4] to check ace
+    userSum = parseInt(userSum) + parseInt(onTable[cardCount].value);   //d
     playerHand.append(onTable[cardCount]);
     cardCount++;
 
@@ -161,40 +174,33 @@ function startGame(){
     }
 
 
-}
+}    
+
+
 //For next log on we need to finish up with setting up clear, hit and the score tracking
 function checkValue(card){
-    let value = card.id.split("-");
-    if(isNaN(value[0])){
-        if(value[0] === 'A'){
+    let value = card.id.split("-"); //splits the card into an arr of [J, C] or [6, D]
+    if(isNaN(value[0])){            //if NaN, then its a K, J, Q, or Ace
+        if(value[0] === 'A'){       //if value is A, then return 11 and that will set ace check to true
             return card.value = 11;
         }
-        else{return card.value = 10}
+        else{return card.value = 10}    //if its not the ace then we will return a 10 since all of them have the same value
     }
     else{
-        return card.value = value[0];
+        return card.value = value[0];   //if its NaN, just return the value of the number
     }
 }
-function handSum(sum, ace){
-    if(sum > 21){
-        if(ace == true){
-            return -10;
-        }  
-    }
-}
-function checkBust(value){
-    if (value > 21 ){
-        return true;
-    }
-}
-let dealerCheck = true;
+
+
 function dealerAuto(){
-    userCheck = false;
+    //resetting vars and buttons
     hitBtn.disabled = true;
     stayBtn.disabled = true;
     stay = true;
+    //shows the hidden card to the user
     onTable[1].src = "/cassino games/cards/"+onTable[1].id+ ".png";
-    
+    //creates new img
+    while(dealerSum < 17){
         onTable[cardCount] = document.createElement("img");
         onTable[cardCount].id = deck.pop();
         onTable[cardCount].src = "/cassino games/cards/"+onTable[cardCount].id+".png";
@@ -205,7 +211,10 @@ function dealerAuto(){
         onTable[cardCount].value = checkAce;
         dealerSum = parseInt(dealerSum) + parseInt(onTable[cardCount].value);
         dealerHand.append(onTable[cardCount]);
+        //gotta check dealer hand
         cardCount++;
+        setTimeout(dealerAuto, 5000);
+    }
       
 }
 
@@ -216,29 +225,32 @@ function dealerAuto(){
 //we go to the dealer Auto and play the dealer auto until the dealer has atleast 17 
 
 function check(){
-    console.log(userSum);
-    console.log(dealerSum + " Dealer");
-    console.log(stay);
     //for user bust
+    console.log(userSum);
+    console.log(dealerSum);
     if(userSum > 21 && stay == false){
         if(userAce == true){
             userSum = parseInt(userSum) - 10;
             userAce = false;
-            return true;
+            return ;
         }
-        onTable[1].src = "/cassino games/cards/"+onTable[1].id+".png";
-        winText.append("House wins...You busted fool");
-        gameOn = false;
-        return false;
+        else{
+            onTable[1].src = "/cassino games/cards/"+onTable[1].id+".png";
+            winText.append("House wins...You busted fool");
+            hitBtn.disabled = true;
+            stayBtn.disabled = true;
+            return;
+        }
+       
     }
-    else if(userSum > dealerSum && dealerSum < 21 && stay== true )
+    else if(userSum > dealerSum && dealerSum < 21 && dealerSum > 17 && stay== true )
     {
         winText.append("User Wins! " + userSum + " is more than " + dealerSum);
-        return false;
+        return;
     }
-    else if(dealerSum > userSum && dealerSum < 21 && stay == true){
+    else if(dealerSum > userSum && dealerSum < 21  && dealerSum > 17 && stay == true){
         winText.append("House Wins!!!");
-        return false;
+        return ;
     }
 
 
